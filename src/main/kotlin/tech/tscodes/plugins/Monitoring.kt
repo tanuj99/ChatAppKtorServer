1b500f04506b8feb2e8dd4a5caf48ae7790a9951
tanuj99 <tanujsoni08@gmail.com>
2022-04-18 00:50:34 +0530

    Add base project structure

--- src/main/kotlin/tech/tscodes/plugins/Monitoring.kt
+++ src/main/kotlin/tech/tscodes/plugins/Monitoring.kt
@@ -1,10 +1,9 @@
 package tech.tscodes.plugins
 
+import io.ktor.application.*
 import io.ktor.features.*
 import org.slf4j.event.*
 import io.ktor.request.*
-import io.ktor.application.*
-import io.ktor.response.*
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
